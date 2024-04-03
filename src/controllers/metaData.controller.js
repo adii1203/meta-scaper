dde20b4c73518a35481272f96f79090885389a9e
adii1203 <aditya32ft@gmail.com>
2024-04-04 00:04:47 +0530

    Fix typo in metaData.controller.js

--- src/controllers/metaData.controller.js
+++ src/controllers/metaData.controller.js
@@ -1,15 +1,14 @@
 import { getMetaData } from "../lib/getMetaData.js";
 import ApiError from "../util/ApiError.js";
 import ApiResponce from "../util/ApiResponce.js";
-import { asuncHandler } from "../util/asyncHandler.js";
+import { asyncHandler } from "../util/asyncHandler.js";
 
-export const handelMetaData = asuncHandler(async (req, res) => {
+export const handelMetaData = asyncHandler(async (req, res) => {
   const { url } = req.query;
   if (!url) throw new ApiError(400, "Url is missing");
 
   try {
     const metadata = await getMetaData(url);
-    console.log(metadata);
 
     if (!metadata) {
       throw new ApiError(404, "No metadata found");
