15c591855ae7b765ed5a7a7c45a67f0aef730e16
adii1203 <aditya32ft@gmail.com>
2024-04-04 00:03:41 +0530

    Add route.js file with metadata and screenshot routes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { handelMetaData } from "../controllers/metaData.controller.js";
+import { handelScreenShot } from "../controllers/screenShot.controllers.js";
 
 const router = Router();
 
 router.route("/metadata").get(handelMetaData);
+router.route("/screenshot").get(handelScreenShot);
 
 export default router;
